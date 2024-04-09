beaab1466fe4a1634ae38ed29eeb3e544d5e320c
Rodrigo Castro <castro.rl@gmail.com>
2024-04-09 13:47:47 -0300

    removing unecessary code

--- admin-ui/src/components/CustomSearchInput.tsx
+++ admin-ui/src/components/CustomSearchInput.tsx
@@ -14,7 +14,6 @@ const CustomSearchInput = ({ onSearch, getSearchByString}: CustomSearchInputProp
   useEffect(() => {
     getSearchByString(client, searchString)
       .then((idsArray: string[]) => {
-        console.log(idsArray);
         onSearch(idsArray);
       })
       .catch((err) => {
