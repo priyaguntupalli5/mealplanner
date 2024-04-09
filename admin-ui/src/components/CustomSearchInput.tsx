fca48312d141302548ab443f88d2bcc1ca4b0e70
Rodrigo Castro <castro.rl@gmail.com>
2024-04-09 13:43:56 -0300

    Abstracting the getSearchByString function to call it from the parent component and pass it as a props to CustomSearchInput component

--- admin-ui/src/components/CustomSearchInput.tsx
+++ admin-ui/src/components/CustomSearchInput.tsx
@@ -1,19 +1,20 @@
-import { useApolloClient } from "@apollo/client";
+import { ApolloClient, useApolloClient } from "@apollo/client";
 import { TextField } from "@mui/material";
 import { useEffect, useState } from "react";
-import { getSearchByString } from "../Products/service";
 
 interface CustomSearchInputProps {
   onSearch: (data: string[]) => void;
+  getSearchByString: (client: ApolloClient<Object>, searchString: string ) => Promise<string[]>;
 }
 
-const CustomSearchInput = ({ onSearch }: CustomSearchInputProps) => {
+const CustomSearchInput = ({ onSearch, getSearchByString}: CustomSearchInputProps) => {
   const client = useApolloClient();
   const [searchString, setSearchString] = useState("");
 
   useEffect(() => {
     getSearchByString(client, searchString)
-      .then((idsArray) => {
+      .then((idsArray: string[]) => {
+        console.log(idsArray);
         onSearch(idsArray);
       })
       .catch((err) => {
