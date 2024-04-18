cc3a7af3d8a865357b390877e8b0464149b746fe
priyaguntupalli5 <guntupallisaipriya@gmail.com>
2024-04-18 16:02:03 -0300

    added new migration

--- backend/db_migrations/000008_match.up.sql
+++ backend/db_migrations/000008_match.up.sql
@@ -37,7 +37,7 @@ comment on function app.product_keyword_ingredients(app.product) is 'Given a pro
 
 create or replace function app.ingredient_keyword_products(i app.ingredient) returns SETOF app.product as $$
     SELECT p.*
-    FROM app.product p where i.product_keyword ilike ANY(p.product_keywords); 
+    FROM app.product p where i.product_keyword = ANY(p.product_keywords); 
 $$ language SQL STABLE;
 comment on function app.ingredient_keyword_products(app.ingredient) is 'Given a product keyword from ingredient, returns a set of products that have the product keyword';
 
