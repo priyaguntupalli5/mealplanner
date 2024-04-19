823c71e661148b7cd79afe1b2bcf16ac2f9cf72f
Rodrigo Castro <castro.rl@gmail.com>
2024-04-19 18:50:37 -0300

    adding new graphql query and function

--- mealplanner-ui/src/state/state.ts
+++ mealplanner-ui/src/state/state.ts
@@ -454,3 +454,32 @@ export const createMealPlan = (input: createMealPlanInput) => {
     });
   });
 };
+
+
+
+const termsAndConditionsGQL = graphql`
+  mutation state_UpdatePersonTermsMutation($personTerms: Boolean!) {
+    updatePersonTerms(input: { personTerms: $personTerms }) {
+      preflight
+    }
+  }
+`;
+
+export const updatePersonTerms = (accepted: boolean) => {
+    return new Promise((res, rej) => {
+      commitMutation(environment, {
+        mutation: termsAndConditionsGQL,
+        variables: {
+          personTerms: accepted,
+        },
+        onCompleted(response, errors) {
+          if (!errors) {
+            res(response);
+            return;
+          }
+          rej(errors);
+        },
+      });
+    });
+  };
+  
