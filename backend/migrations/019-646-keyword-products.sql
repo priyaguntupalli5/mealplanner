8685167c6ee179b6176cb99afb747374b53bda61
priyaguntupalli5 <guntupallisaipriya@gmail.com>
2024-04-24 10:30:29 -0300

    removed the migration file

--- backend/migrations/019-646-keyword-products.sql
+++
@@ -1,5 +0,0 @@
-create or replace function app.ingredient_keyword_products(i app.ingredient) returns SETOF app.product as $$
-    SELECT p.*
-    FROM app.product p where i.product_keyword ilike ANY(p.product_keywords); 
-$$ language SQL STABLE;
-comment on function app.ingredient_keyword_products(app.ingredient) is 'Given a product keyword from ingredient, returns a set of products that have the product keyword';
